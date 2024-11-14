a5bfd05a31bfb5dd711aa1003f943635071bb82e
SheryarAmir <dev.sheryaramir@gmail.com>
2024-11-15 00:27:14 +0500

    make a change in logo and hire me buttton

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -44,14 +44,9 @@ return()=>{
       <div className=" flex items-center h-full justify-between w-[95%] sm-[90%] xl:w-[80%]  mx-auto">
         {/* logo */}
 
-        <Image
-        
-          src="/images/logo.png"
-          alt="LOGO"
-          width={170}
-          height={170}
-          className="ml-[-1.5] sm:ml-0"
-        />
+        <h1 className="font-bold text-2xl text-white">
+  &lt;Code👉.Amir/&gt;
+</h1>
 
         {/* NavLinks */}
         <div className=" flex items-center space-x-10 ">
@@ -68,7 +63,11 @@ return()=>{
           {/* BUTTONS */}
           <div className="flex items-center space-x-4">
 
-<button  className=" md:px-10 md:py-3 px-8 py-3 text-blue-800 font-semibold sm:text-base text-sm bg-white hover:bg-green-500 hover:text-white transition-all duration-200 rounded-lg">Hire Me</button>
+          <Link href="https://www.upwork.com/freelancers/~0170466117f7fadddf">
+  <button className="md:px-10 md:py-3 px-8 py-3 text-blue-800 font-semibold sm:text-base text-sm bg-white hover:bg-green-500 hover:text-white transition-all duration-200 rounded-lg">
+    Hire Me
+  </button>
+</Link>
 
 {/* burger */}
 <HiBars3BottomRight onClick={openNav} className=" w-8 h-8 cursor-pointer text-white lg:hidden" ></HiBars3BottomRight>
